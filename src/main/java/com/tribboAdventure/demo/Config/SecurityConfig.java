35efd0434350c5e333d272ce1dbba1020ca91a4c
leonardofvp <leonardovargasfp@gmail.com>
2023-11-08 19:48:57 -0300

    se añadio config se cors

--- src/main/java/com/tribboAdventure/demo/Config/SecurityConfig.java
+++ src/main/java/com/tribboAdventure/demo/Config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 /**
  *
@@ -46,7 +47,7 @@ public class SecurityConfig {
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         // Agrega tu configuración CORS personalizada
-        //http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
 
         return http.build();
     }
